4e314b5676b6a0357c95ad14c532cc5f758bd464
ammar <ammarraneez@gmail.com>
2023-02-23 17:40:25 +0530

    refactor: remove unneeded files

--- server/utils/constants.py
+++
@@ -1,2 +0,0 @@
-UNIVARIATE_MODEL_PATH = 'D:/Uni/FYP/GitHub/BitForecast/ml/notebooks/model/models/ensemble_univariate'
-MULTIVARIATE_MODEL_PATH = 'D:/Uni/FYP/GitHub/BitForecast/ml/notebooks/model/models/ensemble_multivariate'
