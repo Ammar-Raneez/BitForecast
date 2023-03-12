2559f0f0e9f18cdce3b13bf7998ca338ffdf69bf
ammar <ammarraneez@gmail.com>
2023-03-12 18:18:45 +0530

    Add deployment model to repo

---
+++ deployment/src/pipeline/forecast_pipeline.py
@@ -0,0 +1,11 @@
+def forecase_pipeline(type = 'Multivariate'):
+  pass
+  # Get the dataset
+
+  # Check if dataset is up to date
+
+  # If not fetch new data; add into MongoDB; train model; save model into S3 and local
+
+  # If up to date, use local model to predict
+
+  # Return the prediction
---
+++ deployment/src/pipeline/update_pipeline.py
@@ -0,0 +1,5 @@
+def update_pipeline():
+  pass
+  # Scrape latest data and save into MongoDB
+
+  # Train model; save model into s3; save model locally
